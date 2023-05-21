dde4e818eb142d5b6f26f790a9ee555ace99ead4
Evelyn Ita <evelyn.ita@wemabank.com>
2023-05-21 11:39:31 +0100

    change connection string name & add migration bundle

--- DotNetCoreSqlDb/Data/MyDatabaseContext.cs
+++ DotNetCoreSqlDb/Data/MyDatabaseContext.cs
@@ -16,4 +16,3 @@ namespace DotNetCoreSqlDb.Models
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; }
     }
 }
-// Data Source=evelyn-sql-server.database.windows.net,1433;Initial Catalog=evelyn-sql-database;User ID=evelyn-sql-server-admin;Password=6SF64FWRL000ZQ8M$
